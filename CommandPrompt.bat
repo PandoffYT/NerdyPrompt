ce5446397092ce5ed370fcafeb5508edf971cdd1
Pando_YT <99325649+PandoffYT@users.noreply.github.com>
2024-12-17 21:13:32 +0100

    Imported the file
    Comes with:
    
    - The basic command prompt
    - "parrot" command
    - "spam" command

---
+++ CommandPrompt.bat
@@ -0,0 +1,51 @@
+@echo off
+NET FILE 1>NUL 2>NUL
+IF ERRORLEVEL 1 set elevated=Not elevated& goto CommandPrompt
+set elevated=Elevated
+cls
+echo Command Prompt, Pando, All rights reserved
+echo.
+:CommandPrompt
+title Command prompt - Executing as %username% (%elevated%) in "%~dp0"  (Type "help" to see all useful commands)
+set /p command="%username%@%computername%~ "
+title %command% - Executing as %username% (%elevated%) in "%~dp0"
+echo %username% on %computername% used "%command%" on %time% and %date% at the directory "%~dp0". >> logs.txt
+if "%command%"=="help" goto HelpSection1
+if "%command%"=="parrot" curl parrot.live
+if "%command%"=="spam" goto SpamTool1
+%command%
+goto CommandPrompt
+
+:HelpSection1
+echo "parrot" makes a parrot dance on the screen
+echo "spam" is able to spam create files on a specified path
+pause
+goto CommandPrompt
+
+
+:SpamTool1
+title Spam tool - Executing as %username% (%elevated%) in "%~dp0"  (Type "help" to see all useful commands)
+set CurrentSpamCount=0
+set SpamCount=0
+set SpamLeft=0
+set SpamPath=""
+echo ===================================
+echo.
+echo             Spam tool
+echo            Made by pando
+echo.
+echo ===================================
+echo.
+set /p SpamCount="Enter how many files you want the tool to create: "
+set /p SpamPath="Enter the (full) path where the files are being created (eg. C:/MyFolder/MySubFolder): "
+set /p SpamExtension="Enter the file extension the file should use: "
+:SpamLoop
+echo %random%> %SpamPath%/%random%.%SpamExtension%
+set /a CurrentSpamCount=%CurrentSpamCount%+1
+set /a SpamLeft=%SpamCount%-%CurrentSpamCount%
+title SPAMMING!! (%CurrentSpamCount% in, %SpamLeft% left)
+if %CurrentSpamCount%==%SpamCount% title FINISHED & pause & goto CommandPrompt
+goto SpamLoop
+
+
+
